67e6c722e6894cac5ed1c966f92378aa71d196f6
Nghia Nguyen <nguyenminhnghiaat3@gmail.com>
2025-06-14 09:12:50 +0700

    test

--- c-lab-interview/src/pages/home.tsx
+++ c-lab-interview/src/pages/home.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect } from "react"
 export default function HomePage() {
   return (
     <div className='text-red-500'>
-      this is home page
+      this is home page hello
     </div>
   )
 }
