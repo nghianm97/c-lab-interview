373561c666a9d99861fdea0f24ddbb61f156586d
Nghia Nguyen <nguyenminhnghiaat3@gmail.com>
2025-06-14 08:57:01 +0700

    test

---
+++ c-lab-interview/src/App.tsx
@@ -0,0 +1,11 @@
+import HomePage from './pages/home'
+
+function App() {
+  return (
+    <div className='flex flex-col'>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ c-lab-interview/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function HomePage() {
+  return (
+    <div className='text-red-500'>
+      this is home page
+    </div>
+  )
+}
